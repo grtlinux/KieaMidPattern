004975feaa410d922b6529eba3d81a4917cada90
grtlinux <grtlinux@gmail.com>
2020-07-30 20:43:08 +0900

    add env sources

--- ver02/KieaMid-02/src/main/java/org/tain/KieaMid02Application.java
+++ ver02/KieaMid-02/src/main/java/org/tain/KieaMid02Application.java
@@ -1,13 +1,78 @@
 package org.tain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.tain.utils.CurrentInfo;
+import org.tain.utils.Flag;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-public class KieaMid02Application {
+@EnableScheduling
+@Slf4j
+public class KieaMid02Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get(), LocalDateTime.now());
 		SpringApplication.run(KieaMid02Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+		if (Flag.flag) job01();
+		if (Flag.flag) job02();
+		if (Flag.flag) job03();
+		if (Flag.flag) job04();
+		if (Flag.flag) job05();
+		if (Flag.flag) job06();
+		if (Flag.flag) job07();
+		if (Flag.flag) job08();
+		if (Flag.flag) job09();
+		if (Flag.flag) job10();
+	}
+
+	private void job01() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job02() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job03() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job04() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job05() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job06() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job07() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job08() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job09() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
+
+	private void job10() {
+		log.info("KANG-20200730 >>>>> {} {}", CurrentInfo.get());
+	}
 }
