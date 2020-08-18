2ac09934e3e37c4a9a92bd35e797620f3e628f63
grtlinux <grtlinux@gmail.com>
2020-08-18 23:58:09 +0900

    ...

--- ver05/KieaMid-05/src/main/java/org/tain/repository/mid/CompRepository.java
+++ ver05/KieaMid-05/src/main/java/org/tain/repository/mid/CompRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.tain.domain.mid.Comp;
 import org.tain.info.mid.DivisionCompInfo;
@@ -56,7 +57,25 @@ public interface CompRepository extends JpaRepository<Comp, Long>{
 			+ "from "
 			+ "  Comp c left outer join c.division d "
 			+ "where 1=1 "
-			+ "  and c.id = ?1"
+			//+ "  and c.id = ?1 "
+			+ "  and c.id = :id "
 			)
-	DivisionCompInfo getById(Long id);
+	//DivisionCompInfo getById(Long id);
+	DivisionCompInfo getById(@Param("id") Long id);
+	
+	// Sample
+	@Query(value = ""
+			+ " SELECT "
+			+ "   g.*, "
+			+ "   gm.* "
+			+ " FROM "
+			+ "   group g "
+			+ "   LEFT JOIN group_members gm "
+			+ "     ON g.group_id = gm.group_id "
+			+ "       and gm.user_id = :userId "
+			+ " WHERE 1=1 "
+			+ "   and g.group_id = :groupId "
+			, nativeQuery = true
+			)
+	DivisionCompInfo getGroupDetails(@Param("userId") Integer userId, @Param("groupId") Integer groupId);
 }
